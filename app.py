381f457da518000890c160f2bb043e1178b31f84
HAMMAD <hzmalix@gmail.com>
2022-07-30 14:44:59 +0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return ('Hello API')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
